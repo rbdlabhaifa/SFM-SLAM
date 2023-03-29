e365fe99ef247a7a55e9844f7ea4b76f8ccc84dc
Robotics & Big Data Labs University of Haifa <rbdlabhaifa@gmail.com>
2023-03-29 18:09:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,66 +9,120 @@ class StructureFromMotion:
     def detect_and_describe_keypoints(self):
         """
         Detect keypoints in each image and compute their descriptors using the specified feature detector.
+        
+        Inputs: None (uses self.images and self.feature_detector)
+        Outputs: keypoints_list, descriptors_list (lists of keypoints and descriptors for each image)
         """
         pass
 
-    def match_keypoints(self):
+    def match_keypoints(self, descriptors_list):
         """
         Match keypoints across pairs of images using their descriptors.
+        
+        Inputs: descriptors_list (list of descriptors for each image)
+        Outputs: matches_list (list of matches for each pair of images)
         """
         pass
 
-    def robustly_estimate_matches(self):
+    def robustly_estimate_matches(self, matches_list):
         """
         Apply a robust estimation technique (e.g., RANSAC) to remove outlier matches.
+        
+        Inputs: matches_list (list of matches for each pair of images)
+        Outputs: filtered_matches_list (list of filtered matches for each pair of images)
         """
         pass
 
-    def initialize_structure(self):
+    def initialize_structure(self, keypoints_list, filtered_matches_list):
         """
         Initialize the 3D structure and camera poses with a suitable pair of images.
+        
+        Inputs: keypoints_list (list of keypoints for each image),
+                filtered_matches_list (list of filtered matches for each pair of images)
+        Outputs: initial_structure (initial 3D point cloud),
+                 initial_camera_poses (initial camera poses for the chosen pair of images)
         """
         pass
 
-    def estimate_camera_pose(self):
+    def estimate_camera_pose(self, keypoints, filtered_matches, structure):
         """
         Estimate the camera pose for a new image using PnP (Perspective-n-Point) algorithms.
+        
+        Inputs: keypoints (keypoints of the new image),
+                filtered_matches (filtered matches between the new image and the existing structure),
+                structure (existing 3D point cloud)
+        Outputs: camera_pose (estimated camera pose for the new image)
         """
         pass
 
-    def triangulate_new_points(self):
+    def triangulate_new_points(self, keypoints1, keypoints2, camera_pose1, camera_pose2, matches):
         """
         Triangulate new 3D points from matched keypoints in the new image and the existing 3D structure.
+        
+        Inputs: keypoints1 (keypoints of the new image),
+                keypoints2 (keypoints of the existing structure),
+                camera_pose1 (camera pose of the new image),
+                camera_pose2 (camera pose of the existing structure),
+                matches (filtered matches between the new image and the existing structure)
+        Outputs: new_points (list of newly triangulated 3D points)
         """
         pass
 
-    def update_existing_points(self):
+    def update_existing_points(self, keypoints1, keypoints2, matches, structure):
         """
         Update the existing 3D points with new observations from the new image.
+        
+        Inputs: keypoints1 (keypoints of the new image),
+                keypoints2 (keypoints of the existing structure),
+                matches (filtered matches between the new image and the existing structure),
+                structure (existing 3D point cloud)
+        Outputs: updated_structure (updated 3D point cloud)
         """
         pass
 
-    def bundle_adjustment(self):
+    def bundle_adjustment(self, camera_poses, structure, keypoints_list, filtered_matches_list):
         """
         Apply bundle adjustment to refine the 3D structure and camera poses by minimizing the reprojection error.
+        
+        Inputs: camera_poses (list of camera poses),
+                structure (current 3D point cloud),
+                keypoints_list (list of keypoints for each image),
+                filtered_matches_list (list of filtered matches for each pair of images)
+        Outputs: refined_camera_poses (refined list of camera poses),
+                 refined_structure (refined 3D point cloud)
         """
         pass
 
-    def detect_loop_closure(self):
+    def detect_loop_closure(self, keypoints_list, descriptors_list, camera_poses):
         """
         Detect loop closures, where the camera revisits a previously observed part of the scene.
+
+        Inputs: keypoints_list (list of keypoints for each image),
+                descriptors_list (list of descriptors for each image),
+                camera_poses (list of camera poses)
+        Outputs: loop_closures (list of detected loop closures, each represented by a tuple of image indices)
         """
         pass
 
-    def global_optimization(self):
+    def global_optimization(self, camera_poses, structure, loop_closures):
         """
         Apply global optimization techniques (e.g., global bundle adjustment or pose graph optimization) to correct
         for drift and improve the overall consistency of the reconstruction.
+
+        Inputs: camera_poses (list of camera poses),
+                structure (current 3D point cloud),
+                loop_closures (list of detected loop closures)
+        Outputs: optimized_camera_poses (optimized list of camera poses),
+                 optimized_structure (optimized 3D point cloud)
         """
         pass
 
     def run(self):
         """
         Execute the SfM pipeline by incrementally reconstructing the 3D structure and updating the camera poses.
+
+        Inputs: None (uses self.images and self.feature_detector)
+        Outputs: final_camera_poses (final list of camera poses),
+                 final_structure (final 3D point cloud)
         """
         pass
